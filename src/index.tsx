0a07ab36cba67fa057768cf10ead01650a414ac1
Bjørn Anders Flågen <baflagen@hotmail.com>
2023-01-29 18:05:51 +0100

    Prettier rewrite

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,12 @@ import { OptionsContextProvider } from './hooks/OptionsContext';
 import App from './App';
 import './index.less';
 
-
 // Mock service worker for development
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require( './mocks/browser');
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('./mocks/browser');
   worker.start();
-
 }
 
 const container = document.getElementById('app-root') as Element;
